733b294391315f2bbf63f4a58714376e0f3fd3e4
sorussmann <sofia.russmann@gmail.com>
2019-05-30 19:14:59 -0300

    actualizoscriptlogin

---
+++ js/login.js
@@ -0,0 +1,23 @@
+// INICIAR SESION
+
+const verificarUsuario = function (usuario) {
+//corroborar usuario
+}
+
+const verificarContraseña = function (contraseña) {
+//corroborar contraseña
+}
+
+// REGISTRARSE 
+
+const obtenerUsuario = function (usuario){
+// guardar usuario en base de datos
+}
+
+const obtenerContraseña = function (contraseña){
+// guardar contraseña en base de datos
+}
+
+const obtenerApellido = function (apellido) {
+// guardar apellido en base de datos 
+}
