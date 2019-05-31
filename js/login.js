111441776f27603206f7b30dcd72921092c4a465
sorussmann <sofia.russmann@gmail.com>
2019-05-31 09:40:12 -0300

    comenzandocsslogin

--- js/login.js
+++ js/login.js
@@ -1,11 +1,39 @@
 // INICIAR SESION
 
+let usuariocorrecto = usuario === "adaitw"; //true
+let contraseñacorrecta = contraseña === "2019"; //true
+let resultado = usuariocorrecto && contraseñacorrecta; //true
+
+let actualizarloggueo = function (){
+    document.getElementById('pin').innerHTML = pin;
+}
+
+let resetearloggueo = function (){
+    usuario = "";
+    contraseña = "";
+    actualizarloggueo();
+}
+
 const verificarUsuario = function (usuario) {
+    usuario = prompt ("Ingrese el usuario");
+    actualizarloggueo();
 //corroborar usuario
 }
 
 const verificarContraseña = function (contraseña) {
+    contraseña = prompt ("Ingrese la contraseña");
+    actualizarloggueo();
 //corroborar contraseña
+}
+
+let chequearInicio = function (){
+    if (resultado){
+        alert(`Sesión iniciada correctamente`);
+    }
+    else {
+        alert(`Vuelva a intentarlo.`);
+        resetearloggueo()
+    }
 }
 
 // REGISTRARSE 
